b86fee8d6f3514d92725ffcc540085c8e9c26d39
Alessio G. Baroni <1510959+agbaroni@users.noreply.github.com>
2024-01-24 10:01:02 +0100

    Update main.go

--- main.go
+++ main.go
@@ -3,19 +3,28 @@ package main
 import (
 	"io"
 	"net/http"
+	"net/http/util"
 	"log"
 )
 
 func main() {
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-
-		w.Header().Set("Content-Type", "text/plain")
-
-		_, err := io.WriteString(w, "PING\n")
+		reqDump, err := httputil.DumpRequest(req, true)
 
 		if err != nil {
-			log.Print(err)
+        		log.Print(err)
+    		} else {
+			log.Print(string(reqDump))
+
+			w.WriteHeader(http.StatusOK)
+
+			w.Header().Set("Content-Type", "text/plain")
+
+			_, err := io.WriteString(w, "PING\n")
+
+			if err != nil {
+				log.Print(err)
+			}
 		}
 	})
 
