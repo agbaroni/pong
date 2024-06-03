695d212c0260643eb975f61e59e32951f69b8e29
Alessio G. Baroni <alessiogiovanni.baroni@gmail.com>
2024-06-03 18:00:17 +0200

    Added metrics

--- main.go
+++ main.go
@@ -5,9 +5,31 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"log"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+type metrics struct {
+	n *prometheus.CounterVec
+}
+
+func NewMetrics(reg prometheus.Registerer) *metrics {
+	m := &metrics{
+		n: prometheus.NewCounterVec(prometheus.CounterOpts{
+			Name: "my_counter",
+			Help: "An infinite progressing number",
+		}, []string{"n"}),
+	}
+
+	reg.MustRegister(m.n)
+
+	return m
+}
+
 func main() {
+	reg := prometheus.NewRegistry()
+	m := NewMetrics(reg)
+
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
 		reqDump, err := httputil.DumpRequest(r, true)
 
@@ -25,8 +47,12 @@ func main() {
 			if err != nil {
 				log.Print(err)
 			}
+
+			m.n.With(prometheus.Labels{"n":"PING"}).Inc()
 		}
 	})
+
+	http.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{Registry: reg}))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
